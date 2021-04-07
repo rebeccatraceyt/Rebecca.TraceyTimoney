5565045fb273aeffdc70a54baff7f8472271c2a9
Rebecca Tracey-Timoney <71783662+rebeccatraceyt@users.noreply.github.com>
2021-04-07 19:10:50 +0100

    Fixed page load issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,41 @@
-// --------------------------------------------------------- NavBar
+$(window).on("load", function () {
+  // ------------------------------ NavBar
 
-$("#nav-btn").click(function () {
+  $("#nav-btn").click(function () {
     $(this).toggleClass("animate");
-        if ($(this).hasClass("animate")) {
-            $("#nav-menu").addClass("animate");
-        } else {
-            $("#nav-menu").removeClass("animate");
-        }
-});
-
-// sourced from http://jsfiddle.net/MCka4/
-$("nav a").click(function () {
-  $("#nav-btn").removeClass("animate");
-  $("#nav-menu").removeClass("animate");
-  $("#nav-menu a.active").removeClass("active");
-  $(this).addClass("active");
-});
-
-$(window).scroll(function () {
-  var href = $(this).scrollTop();
-  $(".nav-link").each(function (event) {
-    if (href >= $($(this).attr("href")).offset().top - 1) {
-      $("#nav-menu a.active").removeClass("active");
-      $(this).addClass("active");
+    if ($(this).hasClass("animate")) {
+        $("#nav-menu ul li").css("display", "block");
+      $("#nav-menu").addClass("animate");
+      console.log("SOMETHING");
+    } else {
+      $("#nav-menu").removeClass("animate");
+      console.log("SOMETHING ELSE");
     }
+  });
+
+  // sourced from http://jsfiddle.net/MCka4/
+  $("nav a").click(function () {
+    $("#nav-btn").removeClass("animate");
+    $("#nav-menu").removeClass("animate");
+    $("#nav-menu a.active").removeClass("active");
+    $(this).addClass("active");
+    console.log("SOMETHING SOMETHING ELSE");
+  });
+
+  $(window).scroll(function () {
+    var href = $(this).scrollTop();
+    $(".nav-link").each(function (event) {
+      if (href >= $($(this).attr("href")).offset().top - 1) {
+        $("#nav-menu a.active").removeClass("active");
+        $(this).addClass("active");
+      }
+    });
   });
 });
 
+// ------------------------------ Animate on Scroll
+  AOS.init({
+      duration: 1200,
+    });
 
-// --------------------------------------------------------- Animate on Scroll
-AOS.init({
-  duration: 1200,
-});
+
